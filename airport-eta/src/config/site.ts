e5963c31ee451879b5920b4698f98c65e3650c2a
hk2k1 <m.harshakeerthan@gmail.com>
2024-09-17 02:54:19 +0000

    Frontend Changes
    ShadeCN UI Components src/components/
    
    Theme provider added for dark and light mode themes
    
    Font Changed in layout.tsx with progress bar added

---
+++ airport-eta/src/config/site.ts
@@ -0,0 +1,13 @@
+// import { SiteConfig } from "types"
+
+export const siteConfig = {
+  name: "AirportETA",
+  description:
+    "Airport ETA Proof of Concept Application for predicting taxi waiting times for CAG by NCS.",
+  url: "https://google.com",
+  ogImage: "https://www.shutterstock.com/image-vector/airport-transfer-icon-line-isolated-600nw-1240237735.jpg",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/hk2k1/AirportETA",
+  },
+}
